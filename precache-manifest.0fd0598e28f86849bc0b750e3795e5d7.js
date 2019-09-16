071a683a328994642c64691ce08909d016037094
Darya <mayeternity@mail.ru>
2019-09-16 17:43:19 +0400

    Updates

---
+++ precache-manifest.0fd0598e28f86849bc0b750e3795e5d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0e2bc548a2b58828054a6d43c7cb59",
+    "url": "/React_project/index.html"
+  },
+  {
+    "revision": "be0d4d1c06640096f3d3",
+    "url": "/React_project/static/css/main.b9659bc0.chunk.css"
+  },
+  {
+    "revision": "f5a0218938868eb97b15",
+    "url": "/React_project/static/js/2.b306b657.chunk.js"
+  },
+  {
+    "revision": "be0d4d1c06640096f3d3",
+    "url": "/React_project/static/js/main.38abe222.chunk.js"
+  },
+  {
+    "revision": "1a7d42aeef465e4716d4",
+    "url": "/React_project/static/js/runtime~main.0a3bc5d2.js"
+  }
+]);
