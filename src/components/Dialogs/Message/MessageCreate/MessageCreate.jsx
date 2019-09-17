f574802176c6bc10f42f102de0fa3758df01bf25
Darya <mayeternity@mail.ru>
2019-09-17 12:15:28 +0400

    changes

--- src/components/Dialogs/Message/MessageCreate/MessageCreate.jsx
+++ src/components/Dialogs/Message/MessageCreate/MessageCreate.jsx
@@ -4,39 +4,14 @@ import classes from './MessageCreate.module.css';
 
 const MessageCreate = (props) => {
 
-  return (props.id==1) ?
-  (<div className={classes.message_right}>
+  return (<div  className={(props.id==1) ? classes.message_right : classes.message_left} >
     <div className={classes.message_user}>  
       <img src={props.image} alt="user_dialog"/>
       <p>{props.name}</p>
     </div>
     <div className={classes.message}>{props.message}</div>
  </div>)
- :
- (<div className={classes.message_left}>
-   <div className={classes.message_user}> 
-     <img src={props.image} alt="user_dialog"/>
-     <p>{props.name}</p>
-     </div>
-   <div className={classes.message}>{props.message}</div>   
-</div>)
  ;
-
-
-
-  // return (
-  //   <div className={classes.message_right}>
-  //     <div className={classes.message_user}>
-  //       <img src={props.image} alt="user_dialog" />
-  //       <p>{props.name}</p>
-  //     </div>
-  //     <div className={classes.message}>
-  //       {props.message}
-  //     </div>
-
-  //   </div>)
-
-
 }
 
 export default MessageCreate;
